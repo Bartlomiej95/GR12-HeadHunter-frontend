bce3cf7860e58ae0a78ecf2340b6d25aa41a95ae
HugoBosy <hkliszcz@gmail.com>
2022-07-21 08:23:26 +0200

    page min-width 100vh add hover button & filtr base structure students

--- src/components/Tools/Tools.tsx
+++ src/components/Tools/Tools.tsx
@@ -3,6 +3,7 @@ import {FilterAlt} from "@styled-icons/boxicons-solid";
 import './Tools.css';
 
 export const Tools = () => (
+
     <div className="Tools">
         <input type="search" placeholder="Szukaj"/>
         <button><FilterAlt size={15} color="#4D4D4D"/>Filtrowanie</button>
--- src/pages/AvailableStudents/AvailableStudents.tsx
+++ src/pages/AvailableStudents/AvailableStudents.tsx
@@ -3,17 +3,25 @@ import './AvailableStudents.css';
 import {Menu} from "../../components/Menu/Menu";
 import {Tools} from "../../components/Tools/Tools";
 import {Button} from "../../components/Button/Button";
+import {ChevronDown} from "@styled-icons/boxicons-solid";
 
 export const AvailableStudents = () => {
     return (
         <section>
             <Menu/>
+
             <div className="AvailableStudents">
-
                 <Tools/>
-
-                <Button text="Zarezerwuj miejsce"/>
-
+                <hr/>
+                <div className="students">
+                    <div className="oneStudent">
+                        <p>Jan K.</p>
+                        <div>
+                            <Button text='Zarezerwuj rozmowę'/>
+                            <ChevronDown size={30} color="#666666"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
