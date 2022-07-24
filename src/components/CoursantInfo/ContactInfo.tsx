2f13cd8418b3fb37cd943c394d2fce314d12d145
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2022-07-24 14:42:13 +0200

    CSS styling fix - color variables.

--- src/components/CoursantInfo/ContactInfo.tsx
+++ src/components/CoursantInfo/ContactInfo.tsx
@@ -10,13 +10,13 @@ export const ContactInfo = () => {
             <UserCircle size={150} color="#ddd" className="avatar"/>
             <p className="fullName">Jan Kowalski</p>
             <p className="github">
-                <Github size={22} color="#0B8BD4" className="gh"></Github>jankowalski
+                <Github size={22}/>jankowalski
             </p>
             <p className="phone">
-                <Phone size={22} color="#4d4d4d"/>+48 123 456 789
+                <Phone size={22} className="PhoneIcon"/>+48 123 456 789
             </p>
             <p className="email">
-                <Envelope size={22} color="#4d4d4d"/>jankowalski@gmail.com
+                <Envelope size={22} className="EnvelopeIcon"/>jankowalski@gmail.com
             </p>
             <p className="about">O mnie</p>
             <p className="aboutDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo</p>
